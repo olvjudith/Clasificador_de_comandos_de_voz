fd76cdb94f0cbf16fe5d2fbe47882b76be5bea00
olvjudith <56328905+olvjudith@users.noreply.github.com>
2020-04-26 00:44:52 -0500

    Grabacion_comandos_all

--- Grabacion_comandos.py
+++ Grabacion_comandos.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 25 22:48:47 2020
 
-@author: olvju
-"""
 '''
 https://programacionpython80889555.wordpress.com/2018/10/16/grabacion-de-sonido-con-pyaudio-ejercicio-basico-en-python/
 '''
@@ -27,64 +23,72 @@ RATE=44100
 '''
 44100 frames por segundo
 '''
-duracion=10
+duracion=4
 '''
 Tiempo que  durará el archivo en segundos
 '''
-archivo="grabacion.wav"
-'''
-Asignación del nombre que tendrá el archivo
-'''
-
-audio=pyaudio.PyAudio()
-'''
-Se crea una nueva instancia de la clase py.Audio
-'''
-
-
-stream=audio.open(format=FORMAT,channels=CHANNELS,rate=RATE, input=True,frames_per_buffer=CHUNK)
-'''
-Se inicia pyAudio con el método, se empieza la grabación
-'''
-
-print("grabando...")
-'''
-Permanecerá el mensaje en pantalla durante la ejecución
-del ciclo de lecura y procesamiento de lectura
-'''
-frames=[]
-
-for i in range(0, int(RATE/CHUNK*duracion)):
-    data=stream.read(CHUNK)
-    frames.append(data)
+for i in range(26):
+    archivo="grabacion"+str(i+1)+".wav"
+    '''
+    Asignación del nombre que tendrá el archivo
+    '''
+    audio=pyaudio.PyAudio()
+    '''
+    Se crea una nueva instancia de la clase py.Audio
+    '''
+    stream=audio.open(format=FORMAT,channels=CHANNELS,rate=RATE, input=True,frames_per_buffer=CHUNK)
+    '''
+    Se inicia pyAudio con el método, se empieza la grabación
+    '''
+    print("grabando audio #"+str(i+1))
+    '''
+    Permanecerá el mensaje en pantalla durante la ejecución
+    del ciclo de lecura y procesamiento de lectura
+    '''
+    frames=[]
     
-'''
-Ciclo de lectura de sonido
-'''
-print("grabación terminada")
+    for i in range(0, int(RATE/CHUNK*duracion)):
+        data=stream.read(CHUNK)
+        frames.append(data)
+    '''
+    Ciclo de lectura de sonido
+    '''
+    print("grabación terminada")
+    print("------------------------------------------------------")
+    stream.stop_stream()
+    '''
+    Ordena finalizar el procesado de la información
+    '''
+    stream.close()
+    '''
+    Cierra el procesado de la información
+    '''
+    audio.terminate()
+    '''
+    Finaliza la ejeción del programa
+    '''
+    waveFile = wave.open(archivo, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+    '''
+    Se crea un archivo de audio a partir de la información
+    recolectada en el ciclo for  anterior
+    '''
 
 
-stream.stop_stream()
-'''
-Ordena finalizar el procesado de la información
-'''
-stream.close()
-'''
-Cierra el procesado de la información
-'''
-audio.terminate()
-'''
-Finaliza la ejeción del programa
-'''
 
-waveFile = wave.open(archivo, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
-'''
-Se crea un archivo de audio a partir de la información
-recolectada en el ciclo for  anterior
-'''
+    
+
+
+
+
+
+    
+
+
+
+
 
