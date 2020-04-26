a677f5c6a0b5ad88e93c91cfb6fbde10019b6c0f
olvjudith <56328905+olvjudith@users.noreply.github.com>
2020-04-26 18:59:17 -0500

    archivo_audio_csv

--- voz-texto.py
+++ voz-texto.py
@@ -1,45 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 25 21:24:35 2020
+Created on Sun Apr 26 00:47:59 2020
 
 @author: olvju
 """
-
 import speech_recognition as sr
+import time
 
-r = sr.Recognizer()
-'''
-Se crea una nueva instancia de la clase Recognizer
-'''
+comandos = []
 
-with sr.Microphone() as source:
+for i in range(26):
+        
+    r = sr.Recognizer()
     '''
-    Para que el micrófono por defecto del sistema sea la fuente de
-    datos que vaya a capturar las palabras a decir se ocupa la clase
-    Microphone
+    Se crea una nueva instancia de la clase Recognizer
     '''
-    print("Say something...")
-    '''
-    Mensaje para avisar al usuario que se empieza a
-    capturar el audio
-    '''
-    audio = r.listen(source)
-    '''
-    Para capturar el audio se utiliza
-    el método listen que está presente
-    en la clase Recognizer, esto hasta que
-    se detecte un silencio
-    '''
+
+    with sr.AudioFile('C:\\Users\\olvju\\Documents\\8.Optativa profesionalizante II\\Audios\\grabacion'+str(i+1)+'.wav') as source:
+        '''
+        Para que el archivo .wav sea la fuente de
+        datos que vaya a capturar las palabras a decir se ocupa la clase
+        AudioFile
+        '''
+        audio = r.listen(source)
+        
+        '''
+        Para capturar el audio se utiliza
+        el método listen que está presente
+        en la clase Recognizer, esto hasta que
+        se detecte un silencio
+        '''
     
-    try:
-        text = r.recognize_google(audio, language='es-Es')
-        print("What did you say_ {}".format(text))
-    except:
-        print("I am sorry! I can't understand!")
-    '''
-    El bloque try - except es para controlar
-    aquellos casos donde ocurra un error a la hora de
-    capturar o de entender que hace el usurio
-    '''
-
-'https://www.youtube.com/watch?v=KcjHfnCteZg'
+        try:
+            print("Reading audio file. Please, wait a moment...")
+            text = r.recognize_google(audio, language='es-ES')
+            comandos.append(text)
+            time.sleep(1)
+            print(text)            
+            
+        except:
+            print("I am sorry! I can not understand!")
+            '''
+            El bloque try - except es para controlar
+            aquellos casos donde ocurra un error a la hora de
+            capturar o de entender que hace el usurio
+            '''
+archivo="basededatos.csv"
+csv=open(archivo,"w")
+'''
+En la variable archivo se pone el nombre del archivo .csv
+y se abre, la w es para señalar que se va a escribir en él
+'''
+titulo="Lista de comandos\n"
+csv.write(titulo)
+'''
+Se escribe el título en el archivo .csv
+'''
+for i in range(26):
+    csv.write(comandos[i]+"\n") 
